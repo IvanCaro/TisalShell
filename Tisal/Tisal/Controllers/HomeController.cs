f8e99fbc8d6b30ddb2815b7ec2b89a88cf8b0b3d
Ivan Caro <bonic17@gmail.com>
2012-08-02 19:59:55 -0400

    test a change

--- Tisal/Tisal/Controllers/HomeController.cs
+++ Tisal/Tisal/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Tisal.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to Tisal";
 
             return View();
         }
